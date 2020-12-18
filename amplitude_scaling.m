ffe1c2da9e3a81bb7e1e38d5d1ed6c59dd7f8d31
Lakshay Almadi <48723172+lakshayalmadi@users.noreply.github.com>
2020-12-18 23:33:47 +0530

    Create amplitude_scaling.m

---
+++ amplitude_scaling.m
@@ -0,0 +1,24 @@
+clear all
+clc
+n1 = -1:2;
+x = [2 4 3 1];
+subplot (3,1,1);
+stem(n1,x);
+title ('X Sequence');
+axis ([-3 3 0 5]);
+n2=0:3;
+y = [1 1 2 1];
+subplot (3,1,2);
+stem(n2,y);
+title('Y Sequence');
+axis([-3 3 0 5]);
+n3 = min (min(n1), min ( n2 ) ) : max (max (n1), max ( n2 ));
+s1 = zeros(1,length (n3));
+s2 = s1;
+s1 ( ( n3>=min( n1 )) & (n3 <=max ( n1 ))==1 )=x;
+s2 ( ( n3>=min( n2 )) & (n3 <=max ( n2 ))==1 )=y;
+mul = s1 .* s2;
+subplot(3,1,3)
+stem(n3,mul)
+title('OUTPUT = X*Y');
+axis([-3 3 0 5]);
